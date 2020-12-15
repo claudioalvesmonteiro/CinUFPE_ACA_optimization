3f8226a4261a12047cad62e75229c7903d513d52
Cláudio Monteiro <clam@cin.ufpe.br>
2020-12-15 15:39:21 -0300

    upinit

---
+++ optimization.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+class Search():
+
+
+    def __init__(self):
+
+        # *random generation?* of state
+
+        self.state = []
+        self.state_cost = 0
+
+
+
+    def operator(self):
+        # modify state, changing the order of cities
+    
+
+    def evaluation_function(self):
+        # calculate value of the state
